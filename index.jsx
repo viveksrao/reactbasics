abf40657d59cd2feafd656ec211737a60a0177e0
Vivekanand Rao <vivekanand.rao@gmail.com>
2016-12-22 13:36:20 +0530

    Added index.jsx to act as the entry point for the application. Renamed app.jsx to Scoreboard.jsx and converted it into a Scoreboard component so that it could be exported as a module.

---
+++ index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Scoreboard from './Scoreboard';
+
+ReactDOM.render(
+  <Scoreboard/>,
+  document.getElementById('container')
+);
